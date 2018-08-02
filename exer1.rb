71b6ec1ed2f4215d144dd197e05db55a89d1bef4
“Tyler <tylerpalef@gmail.com>
2018-08-02 12:50:17 -0400

    updated to count for number of words rather than char

--- exer1.rb
+++ exer1.rb
@@ -1,10 +1,15 @@
+# def word_counter(arg)
+#   word_counter = arg
+#   puts word_counter.count "#{arg}"
+# end
+
 def word_counter(arg)
-  word_counter = arg
-  puts word_counter.count "#{arg}"
+  arr = arg.split
+return arr.length
 end
 
-word_counter("Hello world") # returns 2
+puts word_counter("Hello world") # returns 2
 
-word_counter("This is a sentence") # returns 4
+puts word_counter("This is a sentence") # returns 4
 
-word_counter("") # returns 0
+puts word_counter("") # returns 0
